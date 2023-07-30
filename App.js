7f4e11616a3b112cf1eaa0708380609886fbb6ef
Tristan <93832702+cheesetan@users.noreply.github.com>
2023-07-30 11:57:32 +0800

    made ui nice and changed name to Todo App

--- App.js
+++ App.js
@@ -4,9 +4,14 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Enter a goal' style={styles.textInput}/>
-        <Button title="Add goal" />
+        <TextInput placeholder='Enter a todo item' style={styles.textInput} />
+        <Button title="Add item" />
+      </View>
+
+      <View style={styles.listContainer}>
+        <Text>List goes here...</Text>
       </View>
 
       <StatusBar style="auto" />
@@ -16,19 +21,33 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    paddingVertical: 50,
+    flex: 1,
+    paddingTop: 60,
     paddingHorizontal: 15,
   },
 
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    paddingBottom: 15,
+    marginBottom: 15,
   },
 
   textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
+    padding: 10,
+    borderWidth: 1.2,
+    borderColor: "#ccc",
+    height: 40,
     width: "80%",
-    marginRight: "8",
-  }
+    marginRight: 8,
+    borderRadius: 10,
+  },
+
+  listContainer: {
+    flex: 14
+  },
 });
